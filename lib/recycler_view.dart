30a791b13cf5815518fc20e8b2511ea16c6dd199
khorshed <mg6914949@gmail.com>
2024-07-14 04:17:40 +0300

    create search componante

--- lib/recycler_view.dart
+++ lib/recycler_view.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task/constant.dart';
 
 class RecyclerView extends StatelessWidget {
   const RecyclerView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Route',
+          style: TextStyle(
+            color: kPrimaryColor,
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'What do you search for?',
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.only(left: 12),
+                      child: Icon(
+                        Icons.search_rounded,
+                        size: 42,
+                        color: kPrimaryColor,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                      borderSide: const BorderSide(
+                        color: kPrimaryColor,
+                        width: 1.5,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                      borderSide: const BorderSide(
+                        color: kPrimaryColor,
+                        width: 1.5,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              const Icon(
+                Icons.shopping_cart_outlined,
+                size: 32,
+                color: kPrimaryColor,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
